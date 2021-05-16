da5bbcb0f56cfb84d03a696ab9a80066f5989cc0
DoJun Park <dojun527@naver.com>
2021-05-16 14:07:11 +0900

    add route table

--- Terraform/VPC/vpc.tf
+++ Terraform/VPC/vpc.tf
@@ -55,3 +55,53 @@ resource "aws_nat_gateway" "NAT_gateway" {
     Name = "NAT-GW"
   }
 }
+
+
+
+resource "aws_route_table" "public"{
+ vpc_id = aws_vpc.main.id
+
+ route {
+  cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.IGW.id
+ }
+
+ tags = {
+   Name = "terraform-rt-public"
+ }
+
+}
+
+resource "aws_route_table_association" "route_table_association_public"{
+ subnet_id = aws_subnet.public_subnet.id
+ route_table_id = aws_route_table.public.id
+
+}
+
+
+
+resource "aws_route_table" "private"{
+ vpc_id = aws_vpc.main.id
+
+ tags = {
+   Name = "terraform-rt-private"
+ }
+
+}
+
+
+
+resource "aws_route" "private_nat" {
+  route_table_id              = aws_route_table.private.id
+  destination_cidr_block      = "0.0.0.0/0"
+  nat_gateway_id              = aws_nat_gateway.NAT_gateway.id
+}
+
+
+
+
+resource "aws_route_table_association" "route_table_association_private"{
+ subnet_id = aws_subnet.private_subnet.id
+ route_table_id = aws_route_table.private.id
+
+}
