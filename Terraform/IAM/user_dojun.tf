e95420c09288b3bee8faaeecb09b672657dc999b
DoJun Park <dojun527@naver.com>
2021-05-20 12:32:16 +0900

    Add aws_iam_user_policy

--- Terraform/IAM/user_dojun.tf
+++ Terraform/IAM/user_dojun.tf
@@ -1,4 +1,27 @@
 resource "aws_iam_user" "dojun"{
  name = "dojun"
 }
+
+resource "aws_iam_user_policy" "dojun_policy" {
+  name  = "dojun_policy"
+  user  = aws_iam_user.dojun.name
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "*"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
 ~        
